2790c39c3ae5d1228c872496fea818f1b4504fa4
Maissen Belgacem <maissen.developer500@gmail.com>
2024-11-24 14:42:42 +0100

    Almost Finish the 'home-recommendation' component.

--- src/app/pages/home/modules/home-recommendation/home-recommendation.component.ts
+++ src/app/pages/home/modules/home-recommendation/home-recommendation.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BtnFilledComponent } from '../../../../modules/btn-filled/btn-filled.component';
 
 @Component({
   selector: 'app-home-recommendation',
   standalone: true,
-  imports: [],
+  imports: [
+    BtnFilledComponent
+  ],
   templateUrl: './home-recommendation.component.html',
   styleUrl: './home-recommendation.component.scss'
 })
