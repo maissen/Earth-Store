9eeaa692fb31f579bf0f9d8438453192136cf0de
Maissen Belgacem <maissen.developer500@gmail.com>
2024-11-24 15:59:53 +0100

    Almost finish 'app-component-contact' component.

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BtnFilledComponent } from '../../modules/btn-filled/btn-filled.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    BtnFilledComponent
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
