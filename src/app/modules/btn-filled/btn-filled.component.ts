1cceff937cbd045e39561ef82ace019fba94b382
Maissen Belgacem <maissen.developer500@gmail.com>
2024-11-25 14:22:35 +0100

    Create 'btn-filled-component' (functional).

--- src/app/modules/btn-filled/btn-filled.component.ts
+++ src/app/modules/btn-filled/btn-filled.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-btn-filled',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./btn-filled.component.scss']
 })
 export class BtnFilledComponent {
-
+  @Input() text:string = "default text";
+  @Input() link:string = "";
 }
